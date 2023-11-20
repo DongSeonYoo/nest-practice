ef940926d0cfd06df56d04bf1e4729ad973e16f6
ehdtjs0612 <inko51366@naver.com>
2023-11-20 20:15:20 +0900

    base entity 생성

---
+++ src/base.entity.ts
@@ -0,0 +1,17 @@
+import { CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class BaseEntity {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@CreateDateColumn({
+		name: 'created_at'
+	})
+	createdAt: Date;
+
+	@UpdateDateColumn({
+		name: 'updated_at'
+	})
+	updatedAt: Date;
+}
